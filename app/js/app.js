b7a7dc517637b41bef1882d36df478374c9f4b00
Rafael Meza <rhmcord@gmail.com>
2018-02-26 22:35:14 -0500

    Add responsive menu

--- app/js/app.js
+++ app/js/app.js
@@ -5,11 +5,13 @@ import { customSlider }  from './components/customSlider';
 import { sendContactForm } from './components/sendContactForm';
 import { sendWorkWithUsForm } from './components/sendWorkWithUsForm';
 import { addRegexValidation } from './components/formValidation';
+import { responsiveMenu } from './components/responsiveMenu';
 
 document.addEventListener('DOMContentLoaded', (e) => {
   customSlider();
   sendContactForm();
   sendWorkWithUsForm();
   init();
+  responsiveMenu();
   addRegexValidation();
 });
